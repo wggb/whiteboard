cb8baab6d339520ee5b5696917472741731074a9
aeirya <aeirya@gmail.com>
2021-03-02 22:44:26 +0330

    Add select, deselect functions. Add keyboard shortcut for delete, select.
    Co-authored-by: Sadra Saderi <sadrassx@gmail.com>

--- js/whiteboard.js
+++ js/whiteboard.js
@@ -15,11 +15,19 @@ var path;
 
 var clickPoint;
 
+var isBusy = false;
+
 function clearWhiteboard() {
 	for (var i = 0; i < paths.length; i++) {
 		paths[i].remove();
 	}
 	paths = [];
+}
+
+function deselectAll() {
+	for (var i = 0; i < paths.length; i++) {
+		paths[i].selected = false;
+	}
 }
 
 function updateValues() {
@@ -57,63 +65,107 @@ function loadPaths(text) {
 		while (loadedPaths.length > 0) {
 			paths.push(new Path(loadedPaths.shift()[1]));
 		}
-	} catch (error) {
 		alert('Text can\'t be parsed.');
+	} catch (error) {
 	}
-	document.getElementById('save-textarea').value = JSON.stringify(paths);
+}
+
+function drawSelectRectangle(firstPoint, secondPoint) {
+	var rectangle = new Rectangle(firstPoint, secondPoint);
+	var path = new Path.Rectangle(rectangle);
+	path.fillColor = '#eff9ff40';
+	path.selected = true;
+	path.removeOnDrag().removeOnUp();
+	return path;
+}
+
+function selectBox(event) {
+	var rect = drawSelectRectangle(clickPoint, event.point);
+
+	for (var i = 0; i < paths.length; i++) {
+		var p = paths[i];
+		var intersections = rect.getIntersections(p);
+		p.selected = (intersections.length > 0);
+		if (rect.bounds.contains(p.interiorPoint)) {
+			p.selected = true;
+		}
+	}
 }
 
 function onMouseDown(event) {
-	if (path) {
-		path.selected = false;
-	}
+	deselectAll();
+	clickPoint = event.point;
 
 	updateValues();
-
-	if (mode == 'draw') {
-		var pathName = '#' + pathId++;
-		path = new Path({
-			segments: [event.point],
-			strokeColor: color,
-			strokeWidth: Number(width),
-			strokeCap: 'round',
-			name: pathName
-			// fullySelected: true
-		});
-		path.add(event.point);
-	} else if (mode == 'del') {
-		project.activeLayer.selected = false;
-	} else if (mode == 'move') {
-		// Select a point
-		clickPoint = event.point;
+	
+	if (Key.isDown('shift')) {
+		// Nothing
+	} else {
+		isBusy = true;
+		if (mode == 'draw') {
+			var pathName = '#' + pathId++;
+			path = new Path({
+				segments: [event.point],
+				strokeColor: color,
+				strokeWidth: Number(width),
+				strokeCap: 'round',
+				name: pathName
+				// fullySelected: true
+			});
+			path.add(event.point);
+		} else if (mode == 'del') {
+			project.activeLayer.selected = false;
+		}
 	}
 }
 
 function onMouseDrag(event) {
-	if (mode == 'draw') {
-		path.add(event.point);
-	} else if (mode == 'del') {
-		if (event.item) {
-			deletePathFromArray(event.item.name);
-			event.item.remove();
+	if (!isBusy && Key.isDown('shift')) {
+		selectBox(event);
+	} else if (isBusy) {
+		if(mode == 'draw') {
+			path.add(event.point);
+		} else if (mode == 'del') {
+			if (event.item) {
+				deletePathFromArray(event.item.name);
+				event.item.remove();
+			}
+		} else if (mode == 'move') {
+			// Find distance between current point and that point
+			view.center += clickPoint - event.point;
 		}
-	} else if (mode == 'move') {
-		// Find distance between current point and that point
-		view.center += clickPoint - event.point;
 	}
 }
 
 function onMouseUp(event) {
-	if (mode == 'draw') {
-		if(path.segments.length > 5) {
-			path.simplify(10);
+	if (!isBusy && Key.isDown('shift')) {
+		// Nothing
+	} else if (isBusy) {
+		if (mode == 'draw') {
+			if(path.segments.length > 5) {
+				path.simplify(10);
+			}
+			paths.push(path);
 		}
-		paths.push(path);
 	}
 
-	// path.fullySelected = true;
+	isBusy = false;
+	clickPoint = null;
 
 	document.getElementById('save-textarea').value = JSON.stringify(paths);
+}
+
+tool.onKeyDown = function(event) {
+	if (event.key == 'backspace' || event.key == 'delete') {
+		for (var i = 0; i < paths.length; i++) {
+			if (paths[i].selected) {
+				paths[i].remove();
+				deletePathFromArray(paths[i]);
+			}
+		}
+        // Prevent the key event from bubbling
+        return false;
+    }
 }
 
 var doneButton = document.getElementById('save-load-done');
@@ -126,3 +178,8 @@ doneButton.addEventListener('click', function(e) {
 		document.getElementById('load-textarea').value = '';
 	}
 });
+
+/* 
+todo: have array of selected
+	  add constructor functions
+*/
